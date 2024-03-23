406a0e519d45607bd9f92e14a09d71cec3981cf2
Murilo <lilo.marino@hotmail.com>
2024-03-22 22:33:10 -0300

    Criado classes ItemCardapio e Restaurante

--- src/main/java/group/Prova1AdminMuriloMarino/ItemCardapio.java
+++ src/main/java/group/Prova1AdminMuriloMarino/ItemCardapio.java
@@ -1,5 +1,74 @@
 package group.Prova1AdminMuriloMarino;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
 public class ItemCardapio {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @NotBlank(message = "O nome do item do cardápio é obrigatório")
+    private String nome;
+    private String descricao;
+    @NotNull(message = "O preço do item do cardápio é obrigatório")
+    private double preco;
+    @ManyToOne
+    @JoinColumn(name = "id_restaurante", nullable = false)
+    private Restaurante restaurante;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public Restaurante getRestaurante() {
+        return restaurante;
+    }
+
+    public void setRestaurante(Restaurante restaurante) {
+        this.restaurante = restaurante;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || this.getClass() != o.getClass()) {
+            return false;
+        }
+        return ((ItemCardapio) o).id == (this.id);
+    }
+
 }
