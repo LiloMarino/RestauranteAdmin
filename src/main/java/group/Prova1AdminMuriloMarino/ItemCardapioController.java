560d9b89601b6eaa4d893cddb69419ff1681adc4
Murilo <lilo.marino@hotmail.com>
2024-03-23 12:53:55 -0300

    Feito funcionalidade de criar cardápio

--- src/main/java/group/Prova1AdminMuriloMarino/ItemCardapioController.java
+++ src/main/java/group/Prova1AdminMuriloMarino/ItemCardapioController.java
@@ -2,16 +2,42 @@ package group.Prova1AdminMuriloMarino;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import jakarta.validation.Valid;
 
 @Controller
 public class ItemCardapioController {
     @Autowired
     ItemCardapioRepository itemCardapioRepository;
 
-    @GetMapping("/novo-cardapio")
-    public String mostrarNovoCardapio(ItemCardapio cardapio){
+    @Autowired
+    RestauranteRepository  restauranteRepository;
+
+    @GetMapping("/novo-cardapio/{id}")
+    public String mostrarNovoCardapio(ItemCardapio cardapio, @PathVariable("id") int id, Model model){
+        Restaurante restaurante = restauranteRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("ID:" + id));
+        model.addAttribute("restaurante", restaurante);
+        model.addAttribute("cardapio", cardapio); // Porque eu tive que fazer isso?
         return "novo-cardapio";
+    }
+
+    @PostMapping("/adicionar-cardapio/{id}")
+    public String novoCardapio(@PathVariable("id") int id, @Valid ItemCardapio cardapio, BindingResult result) {
+        if (result.hasErrors()) {
+            return "redirect:/novo-cardapio/" + id;
+        }
+        Restaurante restaurante = restauranteRepository.findById(id)
+        .orElseThrow(() -> new IllegalArgumentException("ID:" + id));
+        cardapio.setRestaurante(restaurante);
+
+        itemCardapioRepository.save(cardapio);
+        return "redirect:/cardapio/" + id;
     }
 
 
