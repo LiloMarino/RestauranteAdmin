d5d91bb30bc587f01afc70818180c976765765c2
Murilo <lilo.marino@hotmail.com>
2024-03-23 00:12:59 -0300

    Corrigido erro do Controller

--- src/main/java/group/Prova1AdminMuriloMarino/ItemCardapioController.java
+++ src/main/java/group/Prova1AdminMuriloMarino/ItemCardapioController.java
@@ -1,7 +1,9 @@
 package group.Prova1AdminMuriloMarino;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 
+@Controller
 public class ItemCardapioController {
     @Autowired
     ItemCardapioRepository itemCardapioRepository;
--- src/main/java/group/Prova1AdminMuriloMarino/RestauranteController.java
+++ src/main/java/group/Prova1AdminMuriloMarino/RestauranteController.java
@@ -1,14 +1,19 @@
 package group.Prova1AdminMuriloMarino;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import jakarta.validation.Valid;
+
+@Controller
 public class RestauranteController {
     @Autowired
     RestauranteRepository restauranteRepository;
-    
 
     @GetMapping(value = { "/index", "/" })
     public String mostrarRestaurantes(Model model) {
@@ -18,23 +23,23 @@ public class RestauranteController {
     }
 
     @GetMapping("/novo-restaurante")
-    public String mostrarNovoRestaurante() {
+    public String mostrarNovoRestaurante(Restaurante restaurante) { // Porque precisa desse parâmetro?
         return "novo-restaurante";
     }
 
     @GetMapping("/editar/{id}")
     public String mostrarEditarRestaurante(@PathVariable("id") int id, Model model) {
         Restaurante restaurante = restauranteRepository.findById(id)
-        .orElseThrow(() -> new IllegalArgumentException("ID:" + id));
+                .orElseThrow(() -> new IllegalArgumentException("ID:" + id));
 
         model.addAttribute("restaurante", restaurante);
-        return  "editar-restaurante";
+        return "editar-restaurante";
     }
 
     @GetMapping("/cardapio/{id}")
     public String mostrarCardapioRestaurante(@PathVariable("id") int id, Model model) {
         Restaurante restaurante = restauranteRepository.findById(id)
-        .orElseThrow(() -> new IllegalArgumentException("ID:" + id));
+                .orElseThrow(() -> new IllegalArgumentException("ID:" + id));
 
         model.addAttribute("restaurante", restaurante);
         return "cardapio";
@@ -43,8 +48,18 @@ public class RestauranteController {
     @GetMapping("/remover/{id}")
     public String removerRestaurante(@PathVariable("id") int id) {
         Restaurante restaurante = restauranteRepository.findById(id)
-        .orElseThrow(() -> new IllegalArgumentException("ID:" + id));
+                .orElseThrow(() -> new IllegalArgumentException("ID:" + id));
         restauranteRepository.delete(restaurante);
+        return "redirect:/index";
+    }
+
+    @PostMapping("/adicionar-restaurante")
+    public String novoRestaurante(@Valid Restaurante produto, BindingResult result) {
+        if (result.hasErrors()) {
+            return "/novo-restaurante";
+        }
+
+        restauranteRepository.save(produto);
         return "redirect:/index";
     }
 
